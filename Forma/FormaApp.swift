8f4ab2a2b5f764d1bf100ff0d90a32681fceb83d
va2smoove-code <va2smoove@icloud.com>
2025-11-07 21:59:57 +0100

    Initial Commit

---
+++ Forma/FormaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormaApp.swift
+//  Forma
+//
+//  Created by Valentin Aleksandrov on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct FormaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
